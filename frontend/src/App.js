41c46a0fc3a3d745909d820b6b558dfa791f90c7
Connor <connor.cs@protonmail.com>
2023-02-08 13:55:43 -0500

    popup bug fixed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,12 @@
-import { useState } from "react";
-import ReactMapGL, { Marker, Popup } from "react-map-gl";
+import { useEffect, useState } from "react";
+import { Marker, Popup } from "react-map-gl";
 import Map from "react-map-gl";
 import "./app.css";
 import RoomIcon from "@mui/icons-material/Room";
 import StarIcon from "@mui/icons-material/Star";
 import "mapbox-gl/dist/mapbox-gl.css";
+import axios from "axios";
+
 
 export default function App() {
   const [viewState, setViewState] = useState({
@@ -12,46 +14,87 @@ export default function App() {
     latitude: 40,
     zoom: 3.5,
   });
+  const [pins, setPins] = useState([]);
+  const [currentPlaceId, setCurrentPlaceId] = useState(null);
+  const currentUser = "john";
 
+  useEffect(() => {
+    const getPins = async () => {
+      try {
+        const allPins = await axios.get("/pins");
+        setPins(allPins.data);
+        
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    getPins();
+  }, []);
+
+console.log('curent', currentPlaceId)
   return (
-    <div className="map-container" style={{ height: "100vh", width: "100%" }}>
+    <div className="map-container" style={{ height: "100vh", width: "90%" }}>
       <Map
         {...viewState}
         height="100%"
-        width="100%"
+        width="90%"
         mapboxAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
         onMove={(evt) => setViewState(evt.viewState)}
         mapStyle="mapbox://styles/mapbox/streets-v9"
       >
-        <Marker longitude={-77.050636} latitude={38.889248}>
-          <RoomIcon
-            style={{ fontSize: viewState.zoom * 7, color: "slateblue" }}
-          />
-        </Marker>
-        <Popup
-          longitude={-77.050636}
-          latitude={38.889248}
-          closeButton={true}
-          anchor="left"
-        >
-          <div className="card">
-            <label>Place</label>
-            <h4 className="place">Place name</h4>
-            <label>Review</label>
-            <p className="desc">not badd</p>
-            <label>Rating</label>
-            <div className="stars">
-              <StarIcon className="star" />
-              <StarIcon className="star" />
-            </div>
-            <label>Info</label>
-            <span className="username">
-              Created by <b>john</b>
-            </span>
-            <span className="date">1 hour ago</span>
-          </div>
-        </Popup>
+        {pins.map((p) => (
+            <>
+              <Marker
+              className="marker"
+              longitude={p.long} 
+              latitude={p.lat}
+              offsetLeft={-3.5 * viewState.zoom}
+              offsetTop={-7 * viewState.zoom}>
+                <RoomIcon
+                  style={{
+                    fontSize: viewState.zoom * 7,
+                    color: p.username === currentUser ? "tomato" : "slateblue",
+                    cursor: "pointer",
+                  }}
+                  onClick={() => handleMarkerClick(p._id)}
+                />
+              </Marker>
+              {p._id === currentPlaceId && (
+                <Popup
+                  key={p._id}
+                  longitude={p.long}
+                  latitude={p.lat}
+                  closeButton={true}
+                  closeOnClick={false}
+                  anchor="left"
+                  onClose={() => setCurrentPlaceId('')}
+                >
+                  <div className="card">
+                    <label>Place</label>
+                    <h4 className="place">{p.title}</h4>
+                    <label>Review</label>
+                    <p className="desc">{p.desc}</p>
+                    <label>Rating</label>
+                    <div className="stars">
+                      <StarIcon className="star" />
+                      <StarIcon className="star" />
+                    </div>
+                    <label>Info</label>
+                    <span className="username">
+                      Created by <b>{p.username}</b>
+                    </span>
+                    <span className="date">1 hour ago</span>
+                  </div>
+                </Popup>
+              )}
+            </>
+        ))}
       </Map>
     </div>
   );
+
+  function handleMarkerClick(id) {
+    console.log('id', id)
+    setCurrentPlaceId(id);
+  }
 }
